ff5848531fa24b9dd8cbe9a893540985aa34408a
readul-islam <developer.readul@gmail.com>
2022-06-25 00:48:34 +0600

    k

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const courseRoute = require('./routes/coursesRoute')
+
 
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 
-app.use(courseRoute)
+
 
 
 
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const app = require('./app');
+const courseRoute = require('./routes/coursesRoute')
 const dataConnect = require('./database')
  require('dotenv').config();
  const port = process.env.PORT || 5000;
 
 dataConnect()
-
+app.use(courseRoute)
 app.get('/',(req,res)=>{
 res.send({success:true})
 })
